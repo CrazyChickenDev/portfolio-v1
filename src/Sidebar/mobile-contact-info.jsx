9277d6a67536c87fa956847e55e2ba7b74980c0e
CrazyChickenDev <dannychukz15@gmail.com>
2020-09-20 20:34:17 +0100

    adjusted icon size

--- src/Sidebar/mobile-contact-info.jsx
+++ src/Sidebar/mobile-contact-info.jsx
@@ -40,7 +40,7 @@ const ExpandIcon = styled(SideIcon)`
 	border: 0px solid white;
 	padding: 0;
 	border-radius: 50%;
-	background: palevioletred;
+	background: #ffc200;
 	padding: 10px;
 	z-index: 11;
 	box-shadow: rgba(0, 0, 0, 0.08) 0px 2px 3px 0px, rgba(0, 0, 0, 0.06) 0px 10px 15px 0px;
