2befd7b66636c0ea7c0a06cdfca7da60276f082e
CrazyChickenDev <dannychukz15@gmail.com>
2020-09-20 18:00:40 +0100

    deploy

---
+++ build/precache-manifest.8cfedea955dda14cb5a7ab48e7416e6c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d035ab6adbf06d8338be69a9fbbea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "154399740d6359c7ae48",
+    "url": "/static/css/main.c299df2a.chunk.css"
+  },
+  {
+    "revision": "d0b7396d4b31a4f8395d",
+    "url": "/static/js/2.30234b5b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.30234b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154399740d6359c7ae48",
+    "url": "/static/js/main.4b78127f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "91221022514de693118edf3736ddb0a3",
+    "url": "/static/media/ammo-tank.91221022.png"
+  },
+  {
+    "revision": "6aa229afa231999c36bcab0da0dc1caa",
+    "url": "/static/media/argonauts.6aa229af.png"
+  },
+  {
+    "revision": "099c85499c4dd0714b7118cbbea7780f",
+    "url": "/static/media/bitcoin.099c8549.png"
+  },
+  {
+    "revision": "d57590645caad97c050e8a638b890f8c",
+    "url": "/static/media/burger-app.d5759064.png"
+  },
+  {
+    "revision": "102f697b5623aff0f67c8c1009aa8bf6",
+    "url": "/static/media/candy-shop.102f697b.png"
+  },
+  {
+    "revision": "9126314350e6b6e59bd13b54df6f9e8a",
+    "url": "/static/media/carbon-footprint.91263143.png"
+  },
+  {
+    "revision": "4a9a14d3e69b581dc7ee5b9ef1451a93",
+    "url": "/static/media/city-simulation.4a9a14d3.png"
+  },
+  {
+    "revision": "03228960d4e885274e6bbc63c4499a6d",
+    "url": "/static/media/error-404.03228960.png"
+  },
+  {
+    "revision": "090663aa6a140b6d43cd14ad1c104bcc",
+    "url": "/static/media/machine-learning.090663aa.png"
+  },
+  {
+    "revision": "e2452cc5120697c0116e50d4f546a4d1",
+    "url": "/static/media/marco-polo.e2452cc5.png"
+  },
+  {
+    "revision": "5e9ee377cd4f4505496d5ada801fc953",
+    "url": "/static/media/ninja-simulation.5e9ee377.png"
+  },
+  {
+    "revision": "4f36ab55cf6704379fb5a49848361436",
+    "url": "/static/media/rgb-game.4f36ab55.png"
+  },
+  {
+    "revision": "d00670a61039e488ed972763d94b5731",
+    "url": "/static/media/snake.d00670a6.png"
+  },
+  {
+    "revision": "a97b2defd9f3296a17d511744142cd16",
+    "url": "/static/media/space-bar.a97b2def.png"
+  },
+  {
+    "revision": "a76a50610112fee46b92813b6fddc7e6",
+    "url": "/static/media/to-do.a76a5061.png"
+  },
+  {
+    "revision": "5a69c34fa2b72e60b805390235f9d628",
+    "url": "/static/media/wanteat.5a69c34f.png"
+  },
+  {
+    "revision": "8e26b87376b66c84dba3f9647ce6c5b3",
+    "url": "/static/media/weather.8e26b873.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.82e5ce76494d2871fb570a321c5cbb51.js"
+  "/precache-manifest.8cfedea955dda14cb5a7ab48e7416e6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
