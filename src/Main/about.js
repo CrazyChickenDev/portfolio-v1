9c0fe764d8e19aff3d24ac9eaf5e37d2e8184e26
CrazyChickenDev <dannychukz15@gmail.com>
2020-09-20 10:20:34 +0100

    installed node

--- src/Main/about.js
+++ src/Main/about.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-const Container = styled(motion.section)`
+const Container = styled(motion.section)
+`
 	padding-top: 140px;
 	padding-bottom: 100px;
 	padding-left: 170px;
@@ -23,7 +24,8 @@ const Container = styled(motion.section)`
 	}
 `;
 
-const Title = styled(motion.div)`
+const Title = styled(motion.div)
+`
 	width: 100%;
 	color: white;
 	display: flex;
@@ -36,7 +38,7 @@ const Title = styled(motion.div)`
 	}
 `;
 
-const TitleText = styled.h1`
+const TitleText = styled.h1 `
 	font-size: 32px;
 	padding-right: 0.7em;
 	padding-left: 1em;
@@ -47,7 +49,7 @@ const TitleText = styled.h1`
 	}
 `;
 
-const Line = styled.div`
+const Line = styled.div `
 	content: '';
 	display: block;
 	width: 70%;
@@ -58,7 +60,7 @@ const Line = styled.div`
 	}
 `;
 
-const Body = styled.div`
+const Body = styled.div `
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -68,7 +70,7 @@ const Body = styled.div`
 	}
 `;
 
-const TextContainer = styled.div`
+const TextContainer = styled.div `
 	width: 90%;
 	text-align: center;
 	line-height: 30px;
@@ -81,7 +83,7 @@ const TextContainer = styled.div`
 	}
 `;
 
-const Text = styled.p`
+const Text = styled.p `
 	font-family: ui-monospace;
 	margin: 0;
 	padding-bottom: ${(props) => (props.space ? '1em' : '0')};
@@ -99,7 +101,7 @@ const Text = styled.p`
 	}
 `;
 
-const TextDecoration = styled.p`
+const TextDecoration = styled.p `
 	font-family: fantasy;
 	font-family: Noteworthy;
 	margin: 0;
@@ -118,7 +120,8 @@ const TextDecoration = styled.p`
 	}
 `;
 
-const ParragraphDecoration = styled(TextDecoration)`
+const ParragraphDecoration = styled(TextDecoration)
+`
 	padding: 0;
 	padding-top: 5px;
 	text-align: justify;
@@ -127,65 +130,72 @@ const ParragraphDecoration = styled(TextDecoration)`
 `;
 
 const About = ({ aboutRef }) => {
-	return (
-		<Container ref={aboutRef}>
-			<Title>
-				<TitleText>About Me</TitleText>
-				<Line />
-			</Title>
-			<Body>
-				<TextContainer>
-					<ParragraphDecoration>{'<p>'} </ParragraphDecoration>
-					<Text>
-						I'm currently looking for new opportunities. I'm always open to new projects, ideas, job
-						opportunities and new connections. I love building new stuff so just send me a message and I will
-						answer you 100%.
-					</Text>
-					<ParragraphDecoration>{'</p>'} </ParragraphDecoration>
-					<br />
-					<TextDecoration>{'<br/>'}</TextDecoration>
-					<br />
-					<ParragraphDecoration>{'<p>'} </ParragraphDecoration>
-					<Text>
-						I have a profound passion for programming and new technology. I love to learn new things and build
-						stuff. I studied mechanical engineer although I always like computers, learning programming
-						started as a hobby, until I decided to make the big step and change my whole career into
-						programming.
-					</Text>
-					<ParragraphDecoration>{'</p>'} </ParragraphDecoration>
-					<br />
-					<ParragraphDecoration>{'<p>'} </ParragraphDecoration>
-					<Text>
-						I don't like to quit. Once I start something I finish it, so instead of dropping out of college to
-						pursue my dream of being a programmer, I decided to finish my mechanical engineer degree while
-						learning and working as a programmer.
-					</Text>
-					<ParragraphDecoration>{'</p>'} </ParragraphDecoration>
-					<br />
-					<TextDecoration>{'<br/>'}</TextDecoration>
-					<br />
-					<ParragraphDecoration>{'<p>'} </ParragraphDecoration>
-					<Text>
-						My preferable area is frontend using Reactjs, I love how React makes it so easy to improve the
-						user's experience. As an engineer, I like UX and the balance between UI and UX. I love to build
-						nice looking easy to use responsive web sites. I don't mind getting my hands on other areas if
-						needed like backend but my area of expertise is frontend.
-					</Text>
-					<ParragraphDecoration>{'</p>'} </ParragraphDecoration>
-					<br />
-					<TextDecoration>{'<br/>'}</TextDecoration>
-					<br />
-					<ParragraphDecoration>{'<p>'} </ParragraphDecoration>
-					<Text>
-						Switching carrers is the best decission I have ever made and I am so happy with it. I am a happy
-						person who enjoys work. So my dream company would be one with a really good enviroment with my
-						coworkers.
-					</Text>
-					<ParragraphDecoration>{'</p>'} </ParragraphDecoration>
-				</TextContainer>
-			</Body>
-		</Container>
-	);
+    return ( <
+        Container ref = { aboutRef } >
+        <
+        Title >
+        <
+        TitleText > About Me < /TitleText> <
+        Line / >
+        <
+        /Title> <
+        Body >
+        <
+        TextContainer >
+        <
+        ParragraphDecoration > { '<p>' } < /ParragraphDecoration> <
+        Text >
+        I 'm currently looking for new opportunities. I'
+        m always open to new projects, ideas, job opportunities and new connections.I love building new stuff so just send me a message and I will answer you 100 % . <
+        /Text> <
+        ParragraphDecoration > { '</p>' } < /ParragraphDecoration> <
+        br / >
+        <
+        TextDecoration > { '<br/>' } < /TextDecoration> <
+        br / >
+        <
+        ParragraphDecoration > { '<p>' } < /ParragraphDecoration> <
+        Text >
+        I have a profound passion
+        for programming, security researching and new technology.I love to learn new things and build stuff.I studied computer engineering being that I 've always loved computers, learning programming
+        started as a passion, then I decided to make the big step and move fully into programming. <
+        /Text> <
+        ParragraphDecoration > { '</p>' } < /ParragraphDecoration> <
+        br / >
+        <
+        ParragraphDecoration > { '<p>' } < /ParragraphDecoration> <
+        Text >
+        I don 't like to quit. Once I start something I finish it, so instead of dropping out of college to
+        pursue my dream of being a programmer, I decided to finish my mechanical engineer degree
+        while learning and working as a programmer. <
+        /Text> <
+        ParragraphDecoration > { '</p>' } < /ParragraphDecoration> <
+        br / >
+        <
+        TextDecoration > { '<br/>' } < /TextDecoration> <
+        br / >
+        <
+        ParragraphDecoration > { '<p>' } < /ParragraphDecoration> <
+        Text >
+        My preferable area is frontend using Reactjs, I love how React makes it so easy to improve the user 's experience. As an engineer, I like UX and the balance between UI and UX. I love to build
+        nice looking easy to use responsive web sites.I don 't mind getting my hands on other areas if
+        needed like backend but my area of expertise is frontend. <
+        /Text> <
+        ParragraphDecoration > { '</p>' } < /ParragraphDecoration> <
+        br / >
+        <
+        TextDecoration > { '<br/>' } < /TextDecoration> <
+        br / >
+        <
+        ParragraphDecoration > { '<p>' } < /ParragraphDecoration> <
+        Text >
+        Switching carrers is the best decission I have ever made and I am so happy with it.I am a happy person who enjoys work.So my dream company would be one with a really good enviroment with my coworkers. <
+        /Text> <
+        ParragraphDecoration > { '</p>' } < /ParragraphDecoration> <
+        /TextContainer> <
+        /Body> <
+        /Container>
+    );
 };
 
 export default About;
