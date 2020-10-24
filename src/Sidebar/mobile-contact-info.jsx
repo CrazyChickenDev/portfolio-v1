4e2d0a9f06de4851e3c34110443fdca4962b3f6c
CrazyChickenDev <dannychukz15@gmail.com>
2020-10-24 12:00:18 +0100

    update

--- src/Sidebar/mobile-contact-info.jsx
+++ src/Sidebar/mobile-contact-info.jsx
@@ -81,7 +81,7 @@ const MobileContactInfo = ({ handleToast }) => {
 					animate={expandIcons ? 'show' : 'hidden1'}
 					whileTap={{ scale: 0.85 }}>
 					<a
-						href='https://drive.google.com/file/d/1IjCfJSqBF4yPLEF1eW3ZSNTodt-eSYJp/view?usp=sharing'
+						href='https://drive.google.com/file/d/1iUdfXirmVZJ58RKBDNnUGVGnmKsH7cKB/view?usp=sharing'
 						target='_blank'
 						rel='noopener noreferrer'>
 						<FilePdf />
