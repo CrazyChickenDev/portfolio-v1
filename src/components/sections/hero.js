d71ebdcb6e8f11b4134d98746b645371a0614173
CrazyChickenDev <dannychukz15@gmail.com>
2020-11-22 17:29:24 +0100

    edited contact.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Nwaobi Daniel.</h2>;
+  const three = <h3 className="big-heading">I love breaking into things.</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I'm a computer engineer based in Benin City, NG specializing in building (and occasionally
+      researching) exceptional websites, softwares, scripts and everything in between.
     </p>
   );
   const five = (
